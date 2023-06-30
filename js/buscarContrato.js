06e99856a844dcffc683424489b5ac652f355767
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-30 17:30:15 -0300

    basico-buscar-contrato

--- js/buscarContrato.js
+++ js/buscarContrato.js
@@ -1,6 +1,44 @@
-function buscarContrato(numeroContrato) {
-    // Lógica para buscar o contrato com o número fornecido
-    // ...
-    // Retornar o contrato encontrado ou null caso não seja encontrado
+// Função para capturar os dados do formulário
+function capturarDados() {
+    // Capturar os valores dos campos do formulário
+    const nomeCliente = document.getElementById('nomeCliente').value;
+    const numeroContrato = document.getElementById('numeroContrato').value;
+    const valorContrato = document.getElementById('valorContrato').value;
+    const dataAssinatura = document.getElementById('dataAssinatura').value;
+  
+    // Criar um objeto com os dados capturados
+    const contrato = {
+      nomeCliente: nomeCliente,
+      numeroContrato: numeroContrato,
+      valorContrato: valorContrato,
+      dataAssinatura: dataAssinatura
+    };
+  
+    // Chamar uma função para processar ou armazenar os dados do contrato
+    processarDados(contrato);
   }
+  
+  // Função para processar ou armazenar os dados do contrato
+  function processarDados(contrato) {
+    // Lógica para processar ou armazenar os dados do contrato
+    // Por exemplo, você pode enviar os dados para um servidor, salvar em um banco de dados, etc.
+  
+    // Exemplo de exibição dos dados no console
+    console.log('Dados do contrato:', contrato);
+  
+    // Limpar os campos do formulário
+    document.getElementById('nomeCliente').value = '';
+    document.getElementById('numeroContrato').value = '';
+    document.getElementById('valorContrato').value = '';
+    document.getElementById('dataAssinatura').value = '';
+  
+    // Exibir uma mensagem de sucesso ao usuário
+    alert('Contrato cadastrado com sucesso!');
+  }
+  
+  // Capturar o evento de envio do formulário
+  document.getElementById('formContrato').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impedir o envio do formulário
+    capturarDados(); // Chamar a função para capturar os dados
+  });
   
