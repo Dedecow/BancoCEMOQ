8d3e5b568ff0be2067ed76b57b9ce9bd8497fca6
Andreh Silveira <andrehbahia@gmail.com>
2023-07-01 10:09:42 -0300

    arrumeiImdexEArquivoNovo

---
+++ js/trataEnviaFormulario.js
@@ -0,0 +1,38 @@
+// Selecione o formulário pelo seu ID ou usando outro seletor adequado
+const form = document.getElementById('meuFormulario');
+
+// Adicione um ouvinte de evento para o evento de envio do formulário
+form.addEventListener('submit', function(event) {
+  // Impede o envio padrão do formulário, para evitar o recarregamento da página
+  event.preventDefault();
+
+  // Coleta os valores dos campos do formulário
+  const nomeCliente = document.getElementById('cliente').value;
+  const numeroContrato = document.getElementById('numeroContrato').value;
+  const dataAssinatura = document.getElementById('dataAssinatura').value;
+  const valorContrato = document.getElementById('valorContrato').value;
+  const vencimento = document.getElementById('vencimento').value;
+
+  // Realize a validação dos dados, se necessário
+  if (!nomeCliente || !numeroContrato || !dataAssinatura || !valorContrato || !vencimento) {
+    alert('Por favor, preencha todos os campos do formulário.');
+    return; // Aborta o envio do formulário se algum campo estiver vazio
+  }
+
+  // Execute a lógica de tratamento do envio do formulário aqui
+  // Por exemplo, você pode enviar os dados para um servidor usando AJAX ou realizar outras ações necessárias
+
+  // Exemplo de exibição dos dados coletados no console
+  console.log('Dados do formulário:');
+  console.log('Nome do Cliente:', nomeCliente);
+  console.log('Número do Contrato:', numeroContrato);
+  console.log('Data de Assinatura:', dataAssinatura);
+  console.log('Valor do Contrato:', valorContrato);
+  console.log('Data de Vencimento:', vencimento);
+
+  // Limpe os campos do formulário após o envio, se necessário
+  form.reset();
+
+  // Exiba uma mensagem de sucesso ou redirecione o usuário para outra página, se necessário
+  alert('Formulário enviado com sucesso!');
+});
